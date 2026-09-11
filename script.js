24977f27b81d94acda62d12ec92269a78e933b5c
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-11 13:42:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const getSumBtn = document.createElement("button");
+
 getSumBtn.append("Get Total Price");
+
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+
+    const prices = document.querySelectorAll(".price");
+
+    let total = 0;
+
+    prices.forEach((price) => {
+        total += Number(price.textContent);
+    });
+
+    const table = document.querySelector("table");
+
+    const row = document.createElement("tr");
+    const cell = document.createElement("td");
+
+    cell.textContent = total;
+
+    row.appendChild(cell);
+
+    table.appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
